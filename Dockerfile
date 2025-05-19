02622912dd03fb4befd075fc3df45163eb3cc7ba
Christopher Kodama <ckodama@redhat.com>
2025-05-19 16:23:04 -0400

    move known_hosts to separate file

--- Dockerfile
+++ Dockerfile
@@ -9,8 +9,14 @@ RUN apk add --no-cache \
 
 RUN adduser -D ci
 
+RUN mkdir /home/ci/.ssh
 ADD *.sh /home/ci/
 
+ADD known_hosts /home/ci/.ssh
+RUN chown -R ci: /home/ci
 RUN chmod 555 /home/ci/*.sh 
+RUN chmod 700 /home/ci/.ssh
+
+USER ci
 
 ENTRYPOINT ["/home/ci/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -66,14 +66,8 @@ if [[ -n "$UPSTREAM_SSH_KEY" ]]; then
   echo "$UPSTREAM_SSH_KEY" > $HOME/.ssh/upstream_ssh_key
   chmod 600 $HOME/.ssh/upstream_ssh_key
 
-  cat <<'EOF' > $HOME/.ssh/known_hosts
-github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
-github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=
-github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=
-EOF
-  chmod 644 $HOME/.ssh/known_hosts
   cat $HOME/.ssh/known_hosts  
-  export GIT_SSH_COMMAND="ssh -i ~/.ssh/upstream_ssh_key -o UserKnownHostsFile=$HOME/.ssh/known_hosts"
+  export GIT_SSH_COMMAND="ssh -i ~/.ssh/upstream_ssh_key"
   # convert upstream repo to a ssh-based URI
   UPSTREAM_REPO=$(echo "$UPSTREAM_REPO" | sed -E 's|https://.*github.com/(.*)/(.*).git|git@github.com:\1/\2.git|')
 fi
