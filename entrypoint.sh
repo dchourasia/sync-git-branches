61245e928e816553e58775ff2cc97f5405d15356
Mhamad Chamseddin <mchamsed@redhat.com>
2026-06-09 11:09:39 -0600

    fix conflict resolution by checking CONFLICT before generic merge errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -108,9 +108,7 @@ fi
 
 echo "$MERGE_RESULT"
 
-if [[ $MERGE_RESULT == "" ]] || [[ $MERGE_RESULT == *"merge failed"* ]] || [[ $MERGE_RESULT == *"error:"* ]] || [[ $MERGE_RESULT == *"Aborting"* ]]; then
-  exit 1
-elif [[ $MERGE_RESULT == *"CONFLICT ("* ]]; then
+if [[ $MERGE_RESULT == *"CONFLICT ("* ]]; then
   if [[ -z "$IGNORE_FILES" ]]; then
     echo "Merge conflicts detected and no exclusion patterns to resolve them"
     exit 1
@@ -152,6 +150,8 @@ elif [[ $MERGE_RESULT == *"CONFLICT ("* ]]; then
   echo "All conflicts on excluded files resolved"
   git commit --no-edit -m "Merged upstream"
   git push ${PUSH_ARGS} origin ${DOWNSTREAM_BRANCH} || exit $?
+elif [[ $MERGE_RESULT == "" ]] || [[ $MERGE_RESULT == *"merge failed"* ]] || [[ $MERGE_RESULT == *"error:"* ]] || [[ $MERGE_RESULT == *"Aborting"* ]]; then
+  exit 1
 elif [[ $MERGE_RESULT != *"Already up to date."* ]]; then
   git commit -m "Merged upstream"
   git push ${PUSH_ARGS} origin ${DOWNSTREAM_BRANCH} || exit $?
